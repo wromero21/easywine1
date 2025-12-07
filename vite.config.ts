8f3973f49e5395b66c73e4f28f1201d0b0307905
wromero21 <wesllen45.romero@gmail.com>
2025-12-07 15:33:35 -0300

    Fix typo in import statement for defineConfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineconfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineconfig({
   plugins: [react()],
 })
